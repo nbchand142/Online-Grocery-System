e33cd87887baccf141a55b326283c92ce53e8af2
Naresh Chand <nbchand142@gmail.com>
2022-07-11 14:05:13 +0545

    livewire added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\customer_listController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get("/home",[customer_listController::class,"customer"])->name('homepage');
+//Route::get("/view/{id}",[customer_listController::class,"singal_listing"])->name('single');
+//Route::get("/business/add",[customer_listController::class,"add_business"])->name('add_new');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
