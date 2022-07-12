b41cd08029d7de96b58385f091d6983f3ce60238
Prakash Timilsina <pracasify@gmail.com>
2022-07-12 12:48:05 +0545

    Changing Model Names

--- app/Models/ProductMetaData.php
+++ app/Models/ProductMetaData.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class products_meta_data extends Model
+class ProductMetaData extends Model
 {
     use HasFactory;
     protected $table="products_meta_data";
