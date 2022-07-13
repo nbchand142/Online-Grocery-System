855daa4f47741ba5a387b7bb160af7d00c2b786a
dil air <dilair41@gmail.com>
2022-07-13 13:27:19 +0545

    livewire

---
+++ app/Http/Livewire/HomepageLivewire.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class HomepageLivewire extends Component
+{
+    public function render()
+    {
+        return view('livewire.homepage-livewire');
+    }
+}
