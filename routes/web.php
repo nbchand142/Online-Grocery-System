e86924b48ccea869e0478b82845a5da960173dc7
dil air <dilair41@gmail.com>
2022-07-14 21:28:47 +0545

    routes create

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,40 @@
 
 use App\Http\Livewire\HomepageLivewire;
 use App\Http\Livewire\SingleProductLivewire;
+use App\Http\Livewire\CategoriesLivewire;
+use App\Http\Livewire\CartLivewire;
+use App\Http\Livewire\SearchLivewire;
+use App\Http\Livewire\OrderLivewire;
+use App\Http\Livewire\CheckoutLivewire;
+use App\Http\Livewire\AdminOrderLivewire;
+use App\Http\Livewire\AdminOrderEditLivewire;
+use App\Http\Livewire\AdminProductLivewire;
+
+use App\Http\Livewire\AdminProductEditLivewire;
+use App\Http\Livewire\AdminProductAddLivewire;
+use App\Http\Livewire\AdminUserLivewire;
+use App\Http\Livewire\AdminUserEditLivewire;
+
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',HomepageLivewire::class);
 Route::get('/product/{product_id}',SingleProductLivewire::class);
+Route::get('/categories/{category_id}',CategoriesLivewire::class);
+Route::get('/cart',CartLivewire::class);
+Route::get('/search/{keyword}',SearchLivewire::class);
+Route::get('/my-orders',OrderLivewire::class);
+Route::get('/chekout',CheckoutLivewire::class);
+Route::get('/admin/order/',AdminOrderLivewire::class);
+Route::get('/admin/order/edit/{order_id}',AdminOrderEditLivewire::class);
+Route::get('/admin/products',AdminProductLivewire::class);
+
+Route::get('/admin/product/edit{product_id}',AdminProductEditLivewire::class);
+Route::get('/admin/product/add',AdminProductAddLivewire::class);
+Route::get('/admin/users',AdminUserLivewire::class);
+Route::get('/admin/user/edit{user_id}',AdminUserEditLivewire::class);
+
+
 
 
 Route::middleware([
