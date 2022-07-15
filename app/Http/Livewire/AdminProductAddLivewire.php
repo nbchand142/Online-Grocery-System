ebe996aa59c24b2efccfbe208dd55bb7c8373058
Naresh Chand <nbchand142@gmail.com>
2022-07-15 12:17:46 +0545

    correction

--- app/Http/Livewire/AdminProductAddLivewire.php
+++ app/Http/Livewire/AdminProductAddLivewire.php
@@ -7,6 +7,7 @@ use Livewire\Component;
 class AdminProductAddLivewire extends Component
 {
     function save(){}
+    
     public function render()
     {
         return view('livewire.admin-product-add-livewire');
